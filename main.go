fcc8cf07a13ba029bf3bf3e9fdea7ad78a40702f
浜田　俊祐 <hamashun0820@icloud.com>
2023-12-02 11:41:44 +0900

    feat: クリックで三角を描画できるようにした

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"syscall/js"
 )
 
@@ -13,15 +12,37 @@ func main() {
 
 	bodyW := window.Get("innerWidth").Float()
 	bodyH := window.Get("innerHeight").Float()
-	fmt.Println(bodyW, bodyH)
 	canvasEl.Set("width", bodyW)
 	canvasEl.Set("height", bodyH)
 	window.Call("addEventListener", "resize", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 		bodyW := window.Get("innerWidth").Float()
 		bodyH := window.Get("innerHeight").Float()
-		fmt.Println(bodyW, bodyH)
 		canvasEl.Set("width", bodyW)
 		canvasEl.Set("height", bodyH)
+		return nil
+	}))
+
+	renderTriangle := func(x, y float64) {
+		ctx := canvasEl.Call("getContext", "2d")
+		ctx.Call("beginPath")
+		ctx.Call("moveTo", x, y)
+		ctx.Call("lineTo", x+50, y+25)
+		ctx.Call("lineTo", x+50, y-25)
+		ctx.Set("fillStyle", "#ff0000")
+		ctx.Call("fill")
+		ctx.Call("closePath")
+	}
+
+	clearCanvas := func() {
+		ctx := canvasEl.Call("getContext", "2d")
+		ctx.Call("clearRect", 0, 0, bodyW, bodyH)
+	}
+
+	canvasEl.Call("addEventListener", "click", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		clearCanvas()
+		x := args[0].Get("clientX").Float()
+		y := args[0].Get("clientY").Float()
+		renderTriangle(x, y)
 		return nil
 	}))
 
